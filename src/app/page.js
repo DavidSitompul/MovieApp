797cd0627206916079b9a932cd943774fc08dcea
DavidSitompul <dpaulsitompul@gmail.com>
2024-03-27 22:23:18 +0700

    Fetch data from api recommendations

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import AnimesList from "@/components/AnimesList"
+import { data } from "autoprefixer"
+import React from "react"
+
+const Home = async () => {
+  // server component
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/recommendations/anime`)
+  const animes = await response.json()
+  // console.log(animes.pagination) pagination untuk membagi smua data rekomendasi??
+  // cara menggunakan datanya yaitu  "animes.data"
   return (
-    <>
-      <p>tes</p>
-    </>
+    <div>
+      <div key={data.mal_id} className="grid  px-3 gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-10 lg:grid-cols-5">
+        {animes.data.map((data) => {
+          const [datas] = data.entry //destructering data array
+          console.log(data.content)
+          return (
+            <div key={datas.mal_id}>
+              <AnimesList title={datas.title} img={datas.images.webp.image_url} id={datas.mal_id} content={data.content} />
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
+
+export default Home
