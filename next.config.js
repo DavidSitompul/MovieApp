a5789e990934a30d3242a714ee4d27ff0c3e7097
DavidSitompul <dpaulsitompul@gmail.com>
2024-03-27 22:21:54 +0700

    config image api from hostname: "cdn.myanimelist.net",

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        hostname: "cdn.myanimelist.net",
+      },
+    ],
     domains: ["images.unsplash.com", "images.https://id.pinterest.com/", "images.https://www.google.com/", "images.https://pixabay.com/"],
     unoptimized: true,
   },
