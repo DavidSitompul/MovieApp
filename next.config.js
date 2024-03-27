2fdb88683f5fe969072971f7a22fd1cf77811560
DavidSitompul <dpaulsitompul@gmail.com>
2024-03-27 17:01:27 +0700

    hapus smua file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "images.https://id.pinterest.com/", "images.https://www.google.com/", "images.https://pixabay.com/"],
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
