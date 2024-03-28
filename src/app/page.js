4dac1d6d2931093bdca944b4af339aa5c4d335f2
DavidSitompul <dpaulsitompul@gmail.com>
2024-03-28 22:21:59 +0700

    make component to dinamic and reuseble and mapping data in the component

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,20 @@
-import AnimesList from "@/components/AnimesList"
-import { data } from "autoprefixer"
+import HeaderAnime from "@/components/AnimesList/HeaderAnime"
+import ListRecommend from "@/components/AnimesList/Recommend"
 import React from "react"
 
 const Home = async () => {
   // server component
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/recommendations/anime`)
-  const animes = await response.json()
+  const recommendAnime = await response.json()
   // console.log(animes.pagination) pagination untuk membagi smua data rekomendasi??
-  // cara menggunakan datanya yaitu  "animes.data"
+  // cara menggunakan datanya yaitu  "recommendAnime.data"
   return (
-    <div>
-      <div key={data.mal_id} className="grid  px-3 gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-10 lg:grid-cols-5">
-        {animes.data.map((data) => {
-          const [datas] = data.entry //destructering data array
-          console.log(data.content)
-          return (
-            <div key={datas.mal_id}>
-              <AnimesList title={datas.title} img={datas.images.webp.image_url} id={datas.mal_id} content={data.content} />
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <>
+      <section>
+        <HeaderAnime />
+        <ListRecommend api={recommendAnime} />
+      </section>
+    </>
   )
 }
 
