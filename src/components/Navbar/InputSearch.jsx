4cbb90523db5b73249efdd76ec7bc9ca6fcd5be0
DavidSitompul <dpaulsitompul@gmail.com>
2024-03-29 17:10:32 +0700

    NOT FOUND page

--- src/components/Navbar/InputSearch.jsx
+++ src/components/Navbar/InputSearch.jsx
@@ -8,22 +8,19 @@ const InputSearch = () => {
   const searchRef = useRef()
   const router = useRouter()
   const handleSearch = (e) => {
-    e.preventDefault()
-    const keyword = searchRef.current.value
-    router.push(`/search/${keyword}`)
-  }
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" || e.type === "click") {
+      e.preventDefault()
       const keyword = searchRef.current.value
       router.push(`/search/${keyword}`)
     }
   }
+
   return (
     <div className="relative">
-      <input type="text" placeholder="Cari Anime..." alt="..." className="w-full px-4 h-10 outline-none rounded-md" ref={searchRef} onKeyDown={handleKeyDown} />
+      <input type="text" placeholder="Cari Anime..." alt="..." className="w-full px-4 h-10 outline-none rounded-md" ref={searchRef} onKeyDown={handleSearch} />
 
-      <button onClick={handleSearch}>
-        <CiSearch size={24} className="absolute top-2 end-2" />
+      <button className="absolute top-2 end-2" onClick={handleSearch}>
+        <CiSearch size={24} />
       </button>
     </div>
   )
