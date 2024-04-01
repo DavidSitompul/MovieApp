cefdbdf9596b4229bd648bb748b7a5045b509045
DavidSitompul <dpaulsitompul@gmail.com>
2024-04-01 22:01:49 +0700

    membuat url /anime

--- src/components/AnimesList/PopulerAnime.jsx
+++ src/components/AnimesList/PopulerAnime.jsx
@@ -6,7 +6,7 @@ const PopulerAnime = ({ api }) => {
     <div className="grid px-3 gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-10 lg:grid-cols-5">
       {api.data?.map((data) => {
         return (
-          <Link href={`/${data.mal_id}`} className="" key={data.mal_id}>
+          <Link href={`/anime/${data.mal_id}`} className="" key={data.mal_id}>
             <Card boxShadow="2xl" className="h-[420px]">
               <CardBody>
                 <Image src={data.images.webp.image_url} alt="..." borderRadius="sm" className="h-[200px] w-full hover:scale-[102%] ease-in-out transition-all duration-300" />
