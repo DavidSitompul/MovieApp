a8dd2b4291c25b5971bb8449bd39e07b8db64688
DavidSitompul <dpaulsitompul@gmail.com>
2024-04-01 22:03:03 +0700

    optimasi tampilan videoPlayer

--- src/components/Utilities/videoPlayer.jsx
+++ src/components/Utilities/videoPlayer.jsx
@@ -1,9 +1,16 @@
 "use client"
-import React from "react"
+import { Button } from "@chakra-ui/react"
+import React, { useState } from "react"
 import { IoIosCloseCircleOutline } from "react-icons/io"
+import { FaCirclePlay } from "react-icons/fa6"
+
 import YouTube from "react-youtube"
 
 const VideoPlayer = ({ youtubeId }) => {
+  const [isopen, setIsOpen] = useState(true)
+  const handleVideoPlayer = () => {
+    setIsOpen((prevstate) => !prevstate)
+  }
   const options = {
     width: "300",
     height: "250",
@@ -11,10 +18,18 @@ const VideoPlayer = ({ youtubeId }) => {
   return (
     <>
       <div>
-        <button>
-          <IoIosCloseCircleOutline />
-        </button>
-        <YouTube videoId={youtubeId} onReady={(event) => event.target.pauseVideo()} opts={options} />
+        {isopen ? (
+          <div className="absolute right-0 bottom-0">
+            <button onClick={handleVideoPlayer}>
+              <IoIosCloseCircleOutline size={24} className="bg-red-500 text-white rounded-full" />
+            </button>
+            <YouTube videoId={youtubeId} onReady={(event) => event.target.pauseVideo()} opts={options} />
+          </div>
+        ) : (
+          <Button leftIcon={<FaCirclePlay />} onClick={handleVideoPlayer} colorScheme="teal" variant="solid" size="md">
+            Trailer
+          </Button>
+        )}
       </div>
     </>
   )
