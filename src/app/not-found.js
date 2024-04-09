9aa1b1ee1e612709fd141f7ee1fb4667b9735a81
DavidSitompul <dpaulsitompul@gmail.com>
2024-04-09 10:33:00 +0700

    fixing error miss "alt in image" and spesial character

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -9,7 +9,7 @@ const NotFound = () => {
         <div className="relative">
           <div className="absolute">
             <div className="space-y-3">
-              <Heading className="my-2 text-gray-800 font-bold text-2xl">Looks like you&aposve found the doorway to the great nothing</Heading>
+              <Heading className="my-2 text-gray-800 font-bold text-2xl">Looks like you&apos;ve found the doorway to the great nothing</Heading>
               <Text className="my-2 text-gray-800">Sorry about that! Please visit our hompage to get where you need to go.</Text>
               <Link href="/" className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
                 Back Now!!!!!
