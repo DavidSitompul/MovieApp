7b9aa04def456d94db0208be6eb529b5b5d9e3bb
DavidSitompul <dpaulsitompul@gmail.com>
2024-04-09 15:52:17 +0700

    membuat optional

--- src/components/AnimesList/HotAnime.jsx
+++ src/components/AnimesList/HotAnime.jsx
@@ -6,10 +6,10 @@ import { Card, Text, CardBody, Stack, Heading, Image } from "@chakra-ui/react"
 const HotAnime = ({ api }) => {
   return (
     <>
-      {api.data.map((coming, index) => {
+      {api.data?.map((coming, index) => {
         return (
           <Link href={`/anime/${coming.mal_id}`} className="" key={`${coming.mal_id}_${index}`}>
-            <Card size="sm" boxShadow="2xl" className="w-full mt-5">
+            <Card size="sm" boxShadow="lg" className="w-full mt-5">
               <CardBody display="flex" gap={5}>
                 <Image src={coming.images.webp.image_url} alt="..." borderRadius="sm" className="h-[80px] w-[100px] hover:scale-[102%] ease-in-out transition-all duration-300" />
                 <Stack>
