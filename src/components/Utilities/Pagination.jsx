0b9321fe662cfe17d2d671e431cd894f251e66d3
DavidSitompul <dpaulsitompul@gmail.com>
2024-04-09 13:44:58 +0700

    Memperbaiki posisi button

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -36,30 +36,35 @@ const Pagination = ({ page, lastPage, setPage }) => {
   }
 
   return (
-    <div className="flex flex-col md:flex-row gap-y-7 md:gap-y-0 my-16 justify-center items-center gap-x-6">
-      {page <= 1 ? null : (
-        <>
-          <Button className="items-center flex justify-center" size="md" colorScheme="blue" onClick={handlefirstpage}>
-            1
-          </Button>
-          <Button className="items-center flex justify-center" size="md" colorScheme="blue" onClick={handlePrevPage}>
-            <GrPrevious size={14} />
-          </Button>
-        </>
-      )}
-      <Text className="bg-[#3182CE] p-1  rounded-lg items-center flex justify-center text-white" as="samp" size="md">
-        {page} of {lastPage}
-      </Text>
-      {page >= lastPage ? null : (
-        <>
-          <Button className="items-center flex justify-center" onClick={handleNextPage} size="md" colorScheme="blue">
-            <GrNext />
-          </Button>
-          <Button className="underline underline-offset-4 items-center flex justify-center" size="md" colorScheme="blue" onClick={handlelastpage}>
-            {lastPage}
-          </Button>
-        </>
-      )}
+    <div className="flex flex-col gap-y-7  my-16  items-center gap-x-6">
+      <div className="flex space-x-5">
+        {page <= 1 ? null : (
+          <>
+            <Button className="items-center flex justify-center" size="md" colorScheme="blue" onClick={handlePrevPage}>
+              <GrPrevious size={14} />
+            </Button>
+          </>
+        )}
+        <Text className="bg-[#3182CE] p-1  rounded-lg items-center flex justify-center text-white" as="samp" size="md">
+          {page} of {lastPage}
+        </Text>
+        {page >= lastPage ? null : (
+          <>
+            <Button className="items-center flex justify-center" onClick={handleNextPage} size="md" colorScheme="blue">
+              <GrNext />
+            </Button>
+          </>
+        )}
+      </div>
+
+      <div className="space-x-5">
+        <Button className="items-center flex justify-center" size="md" colorScheme="blue" onClick={handlefirstpage}>
+          Back to page 1
+        </Button>
+        <Button className="items-center flex justify-center" size="md" colorScheme="blue" onClick={handlelastpage}>
+          Next to page {lastPage}
+        </Button>
+      </div>
     </div>
   )
 }
